1759f6cbcd29d756663fba37834259afe10ff646
Slendermanyshka <42029005+Slendermanyshka@users.noreply.github.com>
2023-03-02 23:18:44 +0200

    9 done, started 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsEl = document.getElementById("controls");
+const inputEl = controlsEl.querySelector('input[type=number]');
+const createBtn = document.querySelector('#controls button[data-create');
+const destroyBtn = document.querySelector('#controls button[data-destroy');
+
+createBtn.addEventListener("click", createBoxes)
+destroyBtn.addEventListener("click", destroyBoxes)
+
+function destroyBoxes(event){
+
+}
+
+
+
+function createBoxes(event){
+  const newBoxesList = [];
+  if(inputEl.value!==null){
+    for (let i = 0; i < inputEl.value; i++){
+      newBoxesList.push(getRandomHexColor());
+  }}
+}
+
