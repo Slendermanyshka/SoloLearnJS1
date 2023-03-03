079c391659ad081e8e5607a95995bb03436f54fc
Slendermanyshka <42029005+Slendermanyshka@users.noreply.github.com>
2023-03-03 15:10:31 +0200

    task10 in dz6 is done

--- js/task-10.js
+++ js/task-10.js
@@ -2,6 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const boxesEl=document.getElementById('boxes');
 const controlsEl = document.getElementById("controls");
 const inputEl = controlsEl.querySelector('input[type=number]');
 const createBtn = document.querySelector('#controls button[data-create');
@@ -11,16 +12,36 @@ createBtn.addEventListener("click", createBoxes)
 destroyBtn.addEventListener("click", destroyBoxes)
 
 function destroyBoxes(event){
-
+  boxesEl.innerHTML='';
+  inputEl.value = '';
 }
 
 
 
 function createBoxes(event){
+  let markup="";
   const newBoxesList = [];
   if(inputEl.value!==null){
     for (let i = 0; i < inputEl.value; i++){
-      newBoxesList.push(getRandomHexColor());
-  }}
-}
+      markup+=`<div style="background-color: ${getRandomHexColor()}"></div>`;
+    }}
+
+    boxesEl.insertAdjacentHTML('afterbegin',markup);
+  
+    const arr = [...boxesEl.children];
+    arr.forEach((div,index)=>{
+    div.style.width=`${30+10*(index+1)}px`;
+    div.style.height=`${30+10*(index+1)}px`;
+console.log(div);
+    }
+    );  
+  
+  
+  
+  
+  
+  
+  
+  };
+
 
